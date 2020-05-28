6de7d42d808df3700f71f40d906d0ae562ed4407
Robin Garrison <robin.garrison@adhocteam.us>
2020-05-28 12:34:37 -0500

    Add message for users missing an ID

--- src/applications/disability-benefits/all-claims/Form526EZApp.jsx
+++ src/applications/disability-benefits/all-claims/Form526EZApp.jsx
@@ -7,15 +7,25 @@ import backendServices from 'platform/user/profile/constants/backendServices';
 
 import formConfig from './config/form';
 import ITFWrapper from './containers/ITFWrapper';
-import { MissingServices } from './containers/MissingServices';
+import { MissingServices, MissingId } from './containers/MissingServices';
 
 export const serviceRequired = [
   backendServices.FORM526,
   backendServices.ORIGINAL_CLAIMS,
 ];
 
+export const idRequired = [
+  // checks if EDIPI & SSN exists
+  backendServices.EVSS_CLAIMS,
+  // checks if EDIPI, SSN & either a BIRLS ID or Participant ID exists
+  backendServices.ORIGINAL_CLAIMS,
+];
+
 export const hasRequiredServices = user =>
   serviceRequired.some(service => user.profile.services.includes(service));
+
+export const hasRequiredId = user =>
+  idRequired.some(service => user.profile.services.includes(service));
 
 export function Form526Entry({ location, user, children }) {
   // wraps the app and redirects user if they are not enrolled
@@ -29,10 +39,17 @@ export function Form526Entry({ location, user, children }) {
   if (!user.login.currentlyLoggedIn) {
     return content;
   }
-  // RequiredLoginView checks if you're verified and shows the appropriate link
-  // user 2 doesn't have the required services. Show an alert
-  if (user.profile.verified && !hasRequiredServices(user)) {
-    return <MissingServices />;
+  // RequiredLoginView will handle unverified users by showing the
+  // appropriate link
+  if (user.profile.verified) {
+    // User is missing either their SSN, EDIPI, or BIRLS ID
+    if (!hasRequiredId(user)) {
+      return <MissingId />;
+    }
+    // User doesn't have the required services. Show an alert
+    if (!hasRequiredServices(user)) {
+      return <MissingServices />;
+    }
   }
   return (
     <RequiredLoginView serviceRequired={serviceRequired} user={user} verify>
