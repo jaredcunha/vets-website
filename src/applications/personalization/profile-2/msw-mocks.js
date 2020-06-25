345d458e696e6b4acd66a9abd9547672bbaebd8e
Erik P. Hansen <eph@erikphansen.com>
2020-06-25 11:26:15 -0700

    quick fix attempt

--- src/applications/personalization/profile-2/msw-mocks.js
+++ src/applications/personalization/profile-2/msw-mocks.js
@@ -1,4 +1,5 @@
 import { rest } from 'msw';
+import environment from 'platform/utilities/environment';
 
 export const newPaymentAccount = {
   accountType: 'Savings',
@@ -7,10 +8,12 @@ export const newPaymentAccount = {
   financialInstitutionRoutingNumber: '*****4321',
 };
 
+const prefix = environment.API_URL;
+
 export const updateDirectDepositSuccess = [
   rest.put(
     // I'd prefer to just set the route as `ppiu/payment_information` or at least `v0/ppiu/payment_information`
-    'https://dev-api.va.gov/v0/ppiu/payment_information',
+    `${prefix}/v0/ppiu/payment_information`,
     (req, res, ctx) => {
       return res(
         ctx.json({
@@ -30,33 +33,30 @@ export const updateDirectDepositSuccess = [
 ];
 
 export const updateDirectDepositFailure = [
-  rest.put(
-    'https://dev-api.va.gov/v0/ppiu/payment_information',
-    (req, res, ctx) => {
-      return res.once(
-        ctx.status(402),
-        ctx.json({
-          errors: [
-            {
-              title: 'Unprocessable Entity',
-              detail: 'One or more unprocessable user payment properties',
-              code: '126',
-              source: 'EVSS::PPIU::Service',
-              status: '422',
-              meta: {
-                messages: [
-                  {
-                    key: 'cnp.payment.generic.error.message',
-                    severity: 'ERROR',
-                    text:
-                      'Generic CnP payment update error. Update response: Update Failed: Night area number is invalid, must be 3 digits',
-                  },
-                ],
-              },
+  rest.put(`${prefix}/v0/ppiu/payment_information`, (req, res, ctx) => {
+    return res.once(
+      ctx.status(402),
+      ctx.json({
+        errors: [
+          {
+            title: 'Unprocessable Entity',
+            detail: 'One or more unprocessable user payment properties',
+            code: '126',
+            source: 'EVSS::PPIU::Service',
+            status: '422',
+            meta: {
+              messages: [
+                {
+                  key: 'cnp.payment.generic.error.message',
+                  severity: 'ERROR',
+                  text:
+                    'Generic CnP payment update error. Update response: Update Failed: Night area number is invalid, must be 3 digits',
+                },
+              ],
             },
-          ],
-        }),
-      );
-    },
-  ),
+          },
+        ],
+      }),
+    );
+  }),
 ];
